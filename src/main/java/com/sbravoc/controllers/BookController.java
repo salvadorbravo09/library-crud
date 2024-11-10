1e391887c1b4ee26c74d346b69da2bb0c90350b4
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-10 16:49:06 -0300

    add: book controller with http methods

---
+++ src/main/java/com/sbravoc/controllers/BookController.java
@@ -0,0 +1,73 @@
+package com.sbravoc.controllers;
+
+import com.sbravoc.entities.Book;
+import com.sbravoc.services.BookService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+
+    private BookService bookService;
+
+    // Inyeccion de la interfaz
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> saveBook(@RequestBody Book book) {
+        try {
+            Book savedBook = bookService.saveBook(book);
+            return new ResponseEntity<>(savedBook, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Book> updateBook(@RequestBody Book book) {
+        try {
+            Book savedBook = bookService.updateBook(book);
+            return new ResponseEntity<>(savedBook, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getBooks();
+        if (books == null || books.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(books);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+        if (book.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(book.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+        if (book.isPresent()) {
+            bookService.deleteBook(book.get().getId());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
