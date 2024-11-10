7ec3fc7fd974e1564b55e9831ce48ad74d14fa95
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-09 22:46:31 -0300

    first commit

---
+++ src/test/java/com/sbravoc/LibraryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbravoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
