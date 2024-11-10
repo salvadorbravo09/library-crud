a7081a2b7877f2ff1219e42063b72baac0ad635a
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-10 15:21:34 -0300

    creation of the repository interface for the book entity

---
+++ src/main/java/com/sbravoc/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.sbravoc.repositories;
+
+import com.sbravoc.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
