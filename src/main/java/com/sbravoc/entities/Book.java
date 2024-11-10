4ee60b81fdb61d96f8f534e7f433d7840869233d
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-10 15:19:16 -0300

    creation book entity

---
+++ src/main/java/com/sbravoc/entities/Book.java
@@ -0,0 +1,32 @@
+package com.sbravoc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+
+    @NotNull
+    private Integer pages;
+
+    @NotNull
+    private Double price;
+}
